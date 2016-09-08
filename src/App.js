af34e4d38144de1f438ea18e76ad5594ab12edb0
Ben Godfrey <bcgodfrey91@gmail.com>
2016-09-07 18:37:16 -0600

    Add NavBar functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import firebase from './firebase'
 import SignIn from './SignIn'
+import NavBar from './NavBar'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      user: null;
+      user: null
     }
 }
 
@@ -14,12 +15,15 @@ class App extends Component {
     const { user } = this.state;
     // if (user) {};
     return (
+      <div>
+      <NavBar />
       <section className="sign-in-page">
         <h3>Remembr</h3>
         <article>
           <SignIn />
         </article>
       </section>
+      </div>
     );
   }
 }
