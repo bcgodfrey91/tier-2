5ce30f4b58be58c0fe5e007ef99752d757173186
Adam Bernard <ab255@me.com>
2016-09-12 19:05:32 -0600

    got FB to store id'

--- src/AllInfoCard.js
+++ src/AllInfoCard.js
@@ -1,18 +1,26 @@
 import firebase from './firebase'
 import React, {Component} from 'react';
 import { Link } from 'react-router'
+import UserCard from './UserCard';
+import NewContact from './NewContact';
+
 
 class AllInfoCard extends Component {
-  constructor() {
-    super()
-    this.state = {
-    }
-  }
+
+  
+
 
   render() {
     return(
-      <div>
-        <h2>hello</h2>
+      <div className='all-contact-info' id={this.props.contact.id}>
+        <h1>Full Name: {this.props.contact.fullName} </h1>
+        <h1>Company: {this.props.contact.company} </h1>
+        {/* <h1>E-mail: {this.props.contact.email}</h1>
+        <h1>Phone: {this.props.contact.phone} </h1>
+        <h1>LinkedIn:{this.props.contact.linkedin} </h1>
+        <h1>Twitter: {this.props.contact.twitter} </h1>
+        <h1>URL: {this.props.contact.url}</h1>
+        <h1>Notes: {this.props.contact.notes} </h1> */}
       </div>
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -3,13 +3,14 @@ import { Router, IndexRoute, Route, browserHistory } from 'react-router';
 import Nav from './NavBar'
 
 class Main extends Component {
+
   render(){
     return(
       <div>
-      <Nav />
-      <div>
-      {this.props.children}
-      </div>
+        <Nav />
+        <div>
+          {this.props.children}
+        </div>
       </div>
     )
   }
